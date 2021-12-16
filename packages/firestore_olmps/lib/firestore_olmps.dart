47e44bd0f033b2c36a82bb9ec1992159e29c2100
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-12-16 16:41:14 -0300

    Expose `cloud_firestore` classes for `firestore_olmps`

--- packages/firestore_olmps/lib/firestore_olmps.dart
+++ packages/firestore_olmps/lib/firestore_olmps.dart
@@ -6,3 +6,6 @@ export 'src/defs/query_filter.dart';
 export 'src/defs/query_sort.dart';
 export 'src/firestore_database.dart';
 export 'src/paginated.dart';
+
+export 'package:cloud_firestore/cloud_firestore.dart'
+  show FirebaseFirestore, FieldValue, Blob, GeoPoint, Timestamp;
